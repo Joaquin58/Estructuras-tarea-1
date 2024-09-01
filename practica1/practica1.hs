64c25aa2f612a5ca74290ef8a28c93a787f48e09
Joaquin58 <joaquin2rm@ciencias.unam.mx>
2024-09-01 16:06:00 -0600

    Plantilla de la práctica

---
+++ practica1/practica1.hs
@@ -0,0 +1,51 @@
+-- UNIVERSIDAD NACIONAL AUTÓNOMA DE MEXICO
+-- PRÁCTICA 01: Naturales, inducción y recursión
+-- Ramírez Mendoza Joaquín Rodrigo
+-- Villalobos Juárez Gontran Eliut
+-- Treviño Puebla Héctor Jerome
+
+data Natural = Cero | S Natural deriving Show
+
+-- Práctica Naturales
+-- Definir una funcion ́ a natural que tome un entero (0,1,2,...) y lo lleve a la
+-- estructura de dato Natural.
+
+
+-- Definir una función a_entero que lleve un dato Natural a su vaor entero
+
+
+
+-- Definir una función que realice una suma sobre la estructura de dato Natural
+
+
+
+-- Definir una función que realice la multiplicación de la estructura de un dato Natural
+
+
+--          Práctica recursión
+-- Definir una función que tome como un argumento un entero y regrese el número de Fibonacci correspondiente a ese entero.
+
+
+-- Definir la funcion de multiplicación entre dos enteros de forma recursiva:
+
+
+-- Definir la función de potencia de forma recursiva:
+
+
+--Definir la función factorial de forma recursiva
+
+
+-- Defiinir el algoritmo de la división.
+
+
+-- Define la función maximo de forma recursiva que regrese el valor mas grande de una lista de enteros:
+
+
+
+-- Pŕactica Arboles
+data ArbolB a = Vacio | Nodo a (ArbolB a) (ArbolB a) deriving (Eq, Show)
+
+-- Definir una funcion que calcule el número de nodos del árbol.  ́
+
+
+-- Definir una función que calcule la profundidad de un árbol
