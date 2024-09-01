0350c4c5c2c3dfb2ff0d8a9f05df73e01632ea1c
Joaquin58 <joaquin2rm@ciencias.unam.mx>
2024-09-01 16:09:21 -0600

    agregados ejercicios

--- practica1/practica1.hs
+++ practica1/practica1.hs
@@ -9,28 +9,41 @@ data Natural = Cero | S Natural deriving Show
 -- Práctica Naturales
 -- Definir una funcion ́ a natural que tome un entero (0,1,2,...) y lo lleve a la
 -- estructura de dato Natural.
-
+a_natural :: Int -> Natural
+a_natural 0 = Cero;
+a_natural a = S(a_natural (a-1))
 
 -- Definir una función a_entero que lleve un dato Natural a su vaor entero
-
-
+a_entero :: Natural -> Int
+a_entero Cero = 0
+a_entero (S a) = a_entero a + 1
 
 -- Definir una función que realice una suma sobre la estructura de dato Natural
-
-
+suma_nat :: Natural -> Natural -> Natural
+suma_nat Cero b = b
+suma_nat (S a) b = S(suma_nat a b)
 
 -- Definir una función que realice la multiplicación de la estructura de un dato Natural
-
+mult_nat :: Natural -> Natural -> Natural
+mult_nat b Cero  = Cero
+mult_nat b (S a) = suma_nat (b) (mult_nat b a)
 
 --          Práctica recursión
 -- Definir una función que tome como un argumento un entero y regrese el número de Fibonacci correspondiente a ese entero.
-
+fibonacci :: Int -> Int
+fibonacci 0 = 0
+fibonacci 1 = 1
+fibonacci a = fibonacci(a-1) + fibonacci(a-2)
 
 -- Definir la funcion de multiplicación entre dos enteros de forma recursiva:
-
+mult_ent :: Int -> Int -> Int
+mult_ent a 0 = 0
+mult_ent a b = a + (mult_ent a (b - 1))
 
 -- Definir la función de potencia de forma recursiva:
-
+pot_ent :: Int -> Int -> Int
+pot_ent a 0 = 1
+pot_ent a b = a * (pot_ent a (b-1))
 
 --Definir la función factorial de forma recursiva
 
